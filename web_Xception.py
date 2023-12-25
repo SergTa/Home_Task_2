34d1be9a9b9a3c1fcd1afc6b2fc2bab3707634cc
SergTa <tabakar456@mail.ru>
2023-12-25 13:02:25 +0300

    import tf

--- web_Xception.py
+++ web_Xception.py
@@ -1,9 +1,11 @@
 
+
 import io
 import streamlit as st
 from PIL import Image
 import numpy as np
 import tensorflow 
+from tensorflow import keras
 
 from tensorflow.keras.applications import Xception
 
